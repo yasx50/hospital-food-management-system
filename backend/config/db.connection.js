ad341da3e38c068dd08b7e69145b772508d87fda
yasx50 <yasxcode@gmail.com>
2025-01-09 22:42:32 +0530

    ENV ISSUE

--- backend/config/db.connection.js
+++ backend/config/db.connection.js
@@ -4,7 +4,7 @@ import {DB_NAME} from '../constants.js'
 
 const connectDB = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${'mongodb://localhost:27017'}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log('database connected  ',connectionInstance.connection.host);
         
     } catch (error) {
